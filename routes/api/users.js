5ad10a16be835f480083b35a1d6ec7d6a270d55e
Kyle O'Brien <obrienk@webbhost.net>
2019-06-21 23:42:24 -0500

    added routes and models, beginning working on post for users route

---
+++ routes/api/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+// @route POST api/users
+// @desc Register user
+// @access Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty()
+  ],
+  (req, res) => {
+    console.log(req.body);
+
+    res.send('User route');
+  }
+);
+
+module.exports = router;
