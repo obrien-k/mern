ca36007f73425de2d3770a1d61dbfc764aab6a42
Kyle O'Brien <obrienk@webbhost.net>
2023-06-25 16:15:08 -0500

    update previous routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,19 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const User = require('../../models/User');
 
+// @route GET api/auth
+// @desc Test route
+// @access Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 // @route POST api/users
 // @desc Register user
 // @access Public
@@ -29,7 +42,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = req.body;
+    const { username, email, password } = req.body;
     try {
       let user = await User.findOne({ email });
 
@@ -45,7 +58,7 @@ router.post(
       });
 
       user = new User({
-        name,
+        username,
         email,
         avatar,
         password
@@ -66,7 +79,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
